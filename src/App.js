3aadb5152d8db350807e665fbdfb8687d7b9f46c
IslaMcN <51002520+IslaMcN@users.noreply.github.com>
2019-09-02 11:01:09 -0700

    Done but not sure why it isn't showing up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
 import React from 'react';
+import TodoForm from '../src/components/TodoComponents/TodoForm';
+import TodoList from '../src/components/TodoComponents/TodoList';
+import Title from '../src/components/TodoComponents/Title';
+
+
+window.id = 0;
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor(props){
+    super(props);
+    this.state={
+      data:[]
+    }
+  }
+  addTodo(value){
+    const todo = {text: value, id: window.id++}
+    this.state.data.push(todo);
+    this.setState({data: this.state.data});
+  }
+  handleRemove(id){
+    const remainder = this.state.data.filter((todo) => {
+      if(todo.id !== id) return todo;
+    });
+    this.setState({data: remainder});
+  }
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <Title />
+        <TodoForm addTodo={this.addTodo.bind(this)} />
+        <TodoList
+          todos={this.state.data}
+          remove={this.handleRemove.bind(this)}
+          />
       </div>
     );
   }
 }
 
 export default App;
+
+
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,14 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+import Todo from './Todo';
+
+
+const TodoList = ({todos,remove}) => {
+    const todoNode = todos.map((todo) => {
+        return (<Todo todo={todo} key = {todo.id} remove={remove}/>)
+    });
+    return (<ul>{todoNode}</ul>)
+}
+
+export default TodoList;
