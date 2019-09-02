1473d9a66a7a7c2eec18bcaca32775551a843571
IslaMcN <51002520+IslaMcN@users.noreply.github.com>
2019-09-02 11:41:36 -0700

    Okay I think I made it worse

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import Checkbox from './Checkbox';
 
-const Todo = ({ todo, remove}) => {
-    return ( <li onClick="remove (todo.id)">{todo.text}</li>)
-}
+// const Todo = ({ todo, remove}) => {
+//     return ( <Checkbox onClick = "remove (todo.id)">{todo.text}</Checkbox>)
+// }
 
-export default Todo;
+// export default Todo;
